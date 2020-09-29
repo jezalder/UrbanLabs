6969330dd48adb260c1301d6d44d2700f6f87c77
jezalder <jez.alder@gmail.com>
2020-09-29 13:45:37 +0100

    Update emrr.js

--- sweepDashDemo/emrr.js
+++ sweepDashDemo/emrr.js
@@ -1,1 +1,2 @@
-document.getElementById('ReviewerSearchOptions_ExternalReviewerSearch').checked = true
+document.getElementById('ReviewerSearchOptions_ExternalReviewerSearch').checked = true;
+document.getElementById('ReviewerSearchOptions_ReviewerRoleDropDown').getElementsByTagName('option')[2].selected = 'selected';
