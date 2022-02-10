d0bd41ace6333f5ad5a550b4eb706aaf131f5420
jezalder <jez.alder@gmail.com>
2022-02-10 23:33:16 +0000

    Update emrr.js

--- sweepDashDemo/emrr.js
+++ sweepDashDemo/emrr.js
@@ -1,2 +1,3 @@
 document.getElementById('ReviewerSearchOptions_ExternalReviewerSearch').checked = true;
 document.getElementById('ReviewerSearchOptions_ReviewerRoleDropDown').getElementsByTagName('option')[2].selected = 'selected';
+document.getElementById('userNamePasswordDiv').style.display = "none";
